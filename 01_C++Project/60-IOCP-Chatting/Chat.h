a3bdf3c4efa3e8a2db2b28c46426fb10470c8c6b
egk00 <31694207+egk00@users.noreply.github.com>
2018-09-13 23:52:21 +0900

    채팅 서버 클라이언트
    채팅 서버 클라이언트

---
+++ 01_C++Project/60-IOCP-Chatting/Chat.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <iostream>
+#include <WinSock2.h>
+#pragma comment(lib, "Ws2_32.lib")
+using namespace std;
+
+const char serverIP[] = "127.0.0.1";
+const int serverPort = 12314;
